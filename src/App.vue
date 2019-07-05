c081d753920591023cfade38e8b24d13718317c4
MariahFarnsworth <mariah@thefluent.ca>
2019-07-05 13:36:59 -0400

    Created a Header layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <Header/>
   <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Header from './components/layout/Header.vue';
 import Todos from './components/Todos';
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos,
+    Header
   },
   data(){
     return{
