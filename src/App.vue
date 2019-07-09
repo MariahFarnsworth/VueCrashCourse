720a8a5ad756f48a6299355af7b3183ac006e253
MariahFarnsworth <mariah@thefluent.ca>
2019-07-09 12:51:30 -0400

    items can be add to todo list

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
   <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Header from './components/layout/Header.vue';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'app',
   components: {
     Todos,
-    Header
+    Header,
+    AddTodo
   },
   data(){
     return{
@@ -39,6 +42,9 @@ export default {
   methods: {
     deleteTodo(id){
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
@@ -53,5 +59,17 @@ export default {
   body{
     font-family:Arial, helvetica, sans-serif;
     line-height: 1.4;
+  }
+  .btn{
+    display: inline-block;
+    border: none;
+    background-color:#555;
+    color:#fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover{
+    background:#666;
   }
 </style>
